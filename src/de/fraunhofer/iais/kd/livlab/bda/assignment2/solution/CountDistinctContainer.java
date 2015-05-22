1acfcf5c3ef5e726f6511128b6b7e5872089b3e3
amrqura <amrqura@gmail.com>
2015-05-22 12:32:39 +0200

    task 2 of assignemt 2

---
+++ src/de/fraunhofer/iais/kd/livlab/bda/assignment2/solution/CountDistinctContainer.java
@@ -0,0 +1,42 @@
+package de.fraunhofer.iais.kd.livlab.bda.assignment2.solution;
+
+public class CountDistinctContainer {
+
+	private final String ArtName;
+
+	private boolean isIncreased = false;
+
+	private final CountDistinctSketch sketch = new CountDistinctSketch(
+			Integer.MIN_VALUE);
+
+	public CountDistinctContainer(String ArtName) {
+		this.ArtName = ArtName;
+	}
+
+	public void addUser(String username) {
+		isIncreased = false;
+		int oldEstimation = sketch.getEstimate();
+
+		sketch.addUser(username);
+		if (sketch.getEstimate() != oldEstimation) {
+			isIncreased = true;
+		}
+	}
+
+	public int getCount() {
+		return sketch.getEstimate();
+
+	}
+
+	public boolean isIncreased() {
+		return isIncreased;
+	}
+
+	public CountDistinctSketch getSketch() {
+		return sketch;
+	}
+
+	public String getArtName() {
+		return ArtName;
+	}
+}
